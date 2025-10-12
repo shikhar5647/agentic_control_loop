e27efc1f692daaff7835fc9920486c4887df8d8f
Shikhar Dave <b22ch032@iitj.ac.in>
2025-10-12 23:28:19 +0530

    base_agent

---
+++ src/agents/base_agent.py
@@ -0,0 +1,7 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.schema import HumanMessage, SystemMessage
+from typing import Dict, Any, List
+import logging
+import os
+
+logger = logging.getLogger(__name__)
